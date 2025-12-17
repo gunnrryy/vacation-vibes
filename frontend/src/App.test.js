b13a34db1c07609f60da268974a67ea7b2557dbc
Gunnrryy <gunnrryy@gmail.com>
2025-12-17 14:08:41 +0530

    Updates

--- frontend/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-// test('renders brand name', () => {
-//   render(<App />);
-//   const linkElement = screen.getAllByText(/Vacation Vibes/i)[0];
-//   expect(linkElement).toBeTruthy();
-// });
